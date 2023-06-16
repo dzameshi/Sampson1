f8f63ee631d9297fe60dea5b1d2cf6d5d122792e
dzameshi <136354528+dzameshi@users.noreply.github.com>
2023-06-16 13:11:10 +0000

    Add files via upload

---
+++ lp.py
@@ -0,0 +1,5 @@
+Year=int(input("Enter year: "))
+if Year % 4 == 0:
+    print("YEAR IS A LEAP YEAR")
+else:
+    print("NOT A LEAP YEAR")
---
+++ vt.py
@@ -0,0 +1,6 @@
+YEAR=int(input("ENTER YOUR YEAR OF BIRTH: "))
+CurrentAge=2023-YEAR
+if CurrentAge>=18:
+    print("YOU CAN VOTE")
+else:
+    print("YOU CANNOT VOTE")
